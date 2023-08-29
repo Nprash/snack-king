e9f107b6d595802abc5543641a90aa75271eab50
N.Prasanth <nimmanaprasanthlove@gmail.com>
2023-08-29 09:12:45 +0530

    Restaurants added dynamically

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -11,17 +11,18 @@ const RestaurantMenu = () => {
         fetchMenu();
     },[] );
     const fetchMenu = async ()=>{
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4254486&lng=78.450544&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4254486&lng=78.450544&restaurantId=344287&catalog_qa=undefined&submitAction=ENTER");
         const json = await data.json();
         // console.log(json);
         setRestInfo(json.data)
-        console.log(restInfo)
-    }
+        console.log(json.data)
+    };
+    const {name, cuisines} = restInfo?.cards[0]?.card?.card?.info;
     // if(restInfo === null) return <Shimmer/> with below return
     return restInfo === null? (<Shimmer />):
     (
         <div className="menu">
-            <h1>Name of the Restaurant</h1>
+            <h1>{name}</h1>
             <h2>Menu</h2>
             <ul>
                 <li>Biryani</li>
