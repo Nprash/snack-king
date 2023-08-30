fc33c73f4616c5f520f577f423e7cbd2cf5517ee
N.Prasanth <nimmanaprasanthlove@gmail.com>
2023-08-30 15:29:11 +0530

    Restaurants added dynamically

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -7,25 +7,28 @@ import Shimmer from "./Shimmer";
 const RestaurantMenu = () => {
     const [restInfo, setRestInfo] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchMenu();
-    },[] );
-    const fetchMenu = async ()=>{
+    }, []);
+    const fetchMenu = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4254486&lng=78.450544&restaurantId=344287&catalog_qa=undefined&submitAction=ENTER");
         const json = await data.json();
         // console.log(json);
         setRestInfo(json.data)
         console.log(json.data)
     };
-    const {name, cuisines} = restInfo?.cards[0]?.card?.card?.info;
-    // if(restInfo === null) return <Shimmer/> with below return
-    return restInfo === null? (<Shimmer />):
-    (
+    if (restInfo === null) return (<Shimmer />);
+    // const {name,cuisines, costForTwoMessage, } = restInfo?.cards[0]?.card?.card?.info;
+    const { itemCards } = restInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    console.log(itemCards)
+    return (
         <div className="menu">
-            <h1>{name}</h1>
+            {/* <h1>{name}</h1>
+                <h3>{cuisines}</h3>
+                <h3>{costForTwoMessage}</h3> */}
             <h2>Menu</h2>
             <ul>
-                <li>Biryani</li>
+                <li>{itemCards[0].card.info.name}</li>
                 <li>Burgers</li>
                 <li>Diet Coke</li>
                 <li>Cool Drinks</li>
