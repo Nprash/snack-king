5e22382e0337a4c13d851b2a9dbcc18d5df0607c
N.Prasanth <nimmanaprasanthlove@gmail.com>
2023-09-01 09:39:50 +0530

    Restaurants added dynamically

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -18,17 +18,20 @@ const RestaurantMenu = () => {
         console.log(json.data)
     };
     if (restInfo === null) return (<Shimmer />);
-    // const {name,cuisines, costForTwoMessage, } = restInfo?.cards[0]?.card?.card?.info;
+    // dominos piza api https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4254486&lng=78.450544&restaurantId=24631&catalog_qa=undefined&submitAction=ENTER
+    // old tifins api   https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4254486&lng=78.450544&restaurantId=344287&catalog_qa=undefined&submitAction=ENTER
+    // 2nd restorent ovenstory piza latest API data link: https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4254486&lng=78.450544&restaurantId=25406&catalog_qa=undefined&submitAction=ENTER
+    const {name,cuisines, costForTwoMessage, } = restInfo?.cards[0]?.card?.card?.info;
     const { itemCards } = restInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
     console.log(itemCards)
     return (
         <div className="menu">
-            {/* <h1>{name}</h1>
+            <h1>{name}</h1>
                 <h3>{cuisines}</h3>
-                <h3>{costForTwoMessage}</h3> */}
+                <h3>{costForTwoMessage}</h3>
             <h2>Menu</h2>
             <ul>
-                <li>{itemCards[0].card.info.name}</li>
+                {/* <li>{itemCards[0].card.info.name}</li> */}
                 <li>Burgers</li>
                 <li>Diet Coke</li>
                 <li>Cool Drinks</li>
